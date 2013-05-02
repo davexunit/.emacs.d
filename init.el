d20b1c339a8adc8da83d0a897ae8be43408a3089
David Thompson <dthompson@vistahigherlearning.com>
2013-05-02 08:32:22 -0400

    Remove tromey repo after it downloaded an *ancient* version of ert and screwed up my unit tests.

--- init.el
+++ init.el
@@ -10,8 +10,6 @@
 ;; Extra package repositories.
 (require 'package)
 (add-to-list 'package-archives
-             '("tromey" . "http://tromey.com/elpa/") t)
-(add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
